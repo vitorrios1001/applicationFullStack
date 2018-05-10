21e2af8dcabcb32069d906040228243a622f7949
Vitor Rios <vitorluizrios@hotmail.com>
2018-05-10 02:34:27 -0300

    adding button for Load Products

--- web/src/components/ProductsForm.js
+++ web/src/components/ProductsForm.js
@@ -9,17 +9,17 @@ import fetchProducts from '../actions/productsActions';
 
 class ProductsForm extends Component {
 
-    componentDidMount() {
-        this.props.fetchProducts()
-    }
+    // componentDidMount() {
+    //     this.props.fetchProducts()
+    // }
 
     render() {
-        
+
         if (!this.props.products) {
             console.log('Primeiro if')
 
             return (<h3>Loading...</h3>)
-        } 
+        }
 
         const Products = this.props.products.map(produto => {
             return <ProductItem key={produto._id} item={produto} />
@@ -28,8 +28,13 @@ class ProductsForm extends Component {
         console.log(this.props.products)
 
         return (
-            <div>                
+            <div>
                 <h2>Products</h2>
+                <input
+                    type="submit"
+                    value="Load Products"
+                    onClick={() => this.props.fetchProducts()}
+                />
                 {Products}
             </div>
         )
